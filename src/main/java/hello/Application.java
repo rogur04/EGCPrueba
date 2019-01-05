54e1f0ba435af7dca329afd19e97ddea04707d03
rogur04 <neigmer_14@hotmail.com>
2019-01-05 16:51:53 +0100

    Cambio
    Cambio en español.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class Application {
 
     public static void main(String[] args) {
+	logger.info("Iniciando aplicación...");
         SpringApplication.run(Application.class, args);
     }
 
